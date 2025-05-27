37a15747ff3b2a89b86f2ca7f567f83053ea08bc
aliciarutayugibwa@gmail.com <aliciarutayugibwa@gmail.com>
2025-05-27 16:01:17 +0300

    create a login page for adimn

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+// session_start();
+
+// $admin_user = "admin";
+// $admin_pass = "admin123";
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $user = $_POST['username'] ?? '';
+//     $pass = $_POST['password'] ?? '';
+
+//     if ($user === $admin_user && $pass === $admin_pass) {
+//         // $_SESSION['logged_in'] = true;
+//         echo "success";
+//     } else {
+//         echo "fail";
+//     }
+// } else {
+//     echo"invalid";
+// }
+ print("invalid");
+?>
