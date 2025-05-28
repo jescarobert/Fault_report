d6652890f5fa07f76b79ee0bc697f2ef7f9cb8f6
Jesca Robert <jescarobertjr213@gmail.com>
2025-05-28 07:08:21 +0300

    changes done in php login

--- login.php
+++ login.php
@@ -1,21 +1,16 @@
 <?php
-// session_start();
 
-// $admin_user = "admin";
-// $admin_pass = "admin123";
+$correct_username = "admin";
+$correct_password = "password123";
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $user = $_POST['username'] ?? '';
-//     $pass = $_POST['password'] ?? '';
 
-//     if ($user === $admin_user && $pass === $admin_pass) {
-//         // $_SESSION['logged_in'] = true;
-//         echo "success";
-//     } else {
-//         echo "fail";
-//     }
-// } else {
-//     echo"invalid";
-// }
- print("invalid");
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+
+if ($username === $correct_username && $password === $correct_password) {
+    echo "success";
+} else {
+    echo "fail";
+}
 ?>
